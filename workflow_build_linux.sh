ce1e9a62362ea0121d953a81c58c7bd752f389ce
Daniel Kamil Kozar <dkk089@gmail.com>
2020-01-05 16:40:51 +0100

    Use the debug_and_release qmake config in workflow builds (#12)

--- workflow_build_linux.sh
+++ workflow_build_linux.sh
@@ -4,9 +4,9 @@ set -e
 set -x
 
 compiler=$1
-qmakeflags=(CONFIG+=debug)
+qmakeflags=(CONFIG+=debug_and_release)
 
 mkdir -p "build_${compiler}"
 cd "build_${compiler}"
 qmake ../czateria.pro -spec "linux-${compiler}" "${qmakeflags[@]}"
-make -j$(grep -c ^processor /proc/cpuinfo)
+make -j$(grep -c ^processor /proc/cpuinfo) all
--- workflow_build_windows.bat
+++ workflow_build_windows.bat
@@ -1,5 +1,5 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 mkdir build
 cd build
-qmake ..\czateria.pro -spec win32-msvc CONFIG+=debug
-nmake
+qmake ..\czateria.pro -spec win32-msvc
+nmake all
