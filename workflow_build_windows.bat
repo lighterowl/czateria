d818b5826b9a50c1af4b376ab25226cd4f5aa192
Daniel Kamil Kozar <dkk089@gmail.com>
2020-02-01 11:06:24 +0100

    Use jom when building for Windows

--- workflow_build_windows.bat
+++ workflow_build_windows.bat
@@ -1,5 +1,6 @@
+choco install jom
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 mkdir build
 cd build
 qmake ..\czateria.pro -spec win32-msvc
-nmake all
+jom all
