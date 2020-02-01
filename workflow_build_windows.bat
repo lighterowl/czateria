c32351ba904600d18963a0c5e7840384c39626cd
Daniel Kamil Kozar <dkk089@gmail.com>
2020-02-01 13:38:01 +0100

    let's try that

--- workflow_build_windows.bat
+++ workflow_build_windows.bat
@@ -1,6 +1,7 @@
-choco install jom
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 mkdir build
 cd build
+wget http://download.qt.io/official_releases/jom/jom.zip
+7z x jom.zip
 qmake ..\czateria.pro -spec win32-msvc
 jom all
