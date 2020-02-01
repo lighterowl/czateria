eabc5dbbb09e10d9f6d180a8c187ed98b924bd0a
Daniel Kamil Kozar <dkk089@gmail.com>
2020-02-01 13:46:43 +0100

    huh

--- workflow_build_windows.bat
+++ workflow_build_windows.bat
@@ -1,7 +1,7 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 mkdir build
 cd build
-wget http://download.qt.io/official_releases/jom/jom.zip
+powershell Invoke-WebRequest -Uri http://download.qt.io/official_releases/jom/jom.zip -OutFile jom.zip
 7z x jom.zip
 qmake ..\czateria.pro -spec win32-msvc
 jom all
