e476a7529ee7d022b9591f470c0fc4729bf36373
Daniel Kamil Kozar <dkk089@gmail.com>
2020-03-13 15:21:53 +0100

    Use version 1.2 of qt563xp

--- workflow_build_winxp.bat
+++ workflow_build_winxp.bat
@@ -1,6 +1,6 @@
 
 mkdir C:\Qt\
-powershell -Command "Invoke-WebRequest https://github.com/xavery/qt563xp/releases/download/1.1/Qt-5.6.3-Static-XP.7z -O qt-xp-static.7z"
+powershell -Command "Invoke-WebRequest https://github.com/xavery/qt563xp/releases/download/1.2/Qt-5.6.3-Static-XP.7z -O qt-xp-static.7z"
 7z x -oC:\Qt\ qt-xp-static.7z
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
