6443c318290682fec04aa226947f28e41d421f2d
Daniel Kamil Kozar <dkk089@gmail.com>
2020-09-04 01:32:56 +0200

    Make MainWindow::AutologinState a QObject (#84)
    Fixes #83.

--- ui/mainwindow.cpp
+++ ui/mainwindow.cpp
@@ -91,7 +91,7 @@ const QValidator *getNicknameValidator() {
 constexpr auto channelListRefreshInterval = 5 * 60 * 1000;
 } // namespace
 
-class MainWindow::AutologinState {
+class MainWindow::AutologinState : public QObject {
   // a helper class introduced in order not to clutter the MainWindow object
   // with the state variables while performing autologin, mostly needed in order
   // to display warning messages when a given channel cannot be joined.
@@ -113,7 +113,7 @@ private:
     auto rooms = mLoginHash.values(*mLoginIter);
 
     oneshotConnect(
-        session, &Czateria::LoginSession::loginSuccessful, session, [=]() {
+        session, &Czateria::LoginSession::loginSuccessful, this, [=]() {
           auto ses = QSharedPointer<Czateria::LoginSession>(session);
           for (auto roomId : rooms) {
             if (auto room = mMainWindow->mRoomListModel->roomFromId(roomId)) {
@@ -122,7 +122,7 @@ private:
           }
           nextSession();
         });
-    connect(session, &Czateria::LoginSession::loginFailed, session, [=]() {
+    connect(session, &Czateria::LoginSession::loginFailed, this, [=]() {
       QMessageBox::warning(mMainWindow, tr("Autologin failed"),
                            tr("Autologin failed for username %1.\nRooms "
                               "using this username will not be autojoined.")
@@ -152,7 +152,7 @@ private:
     if (mLoginIter != std::cend(mUniqueLogins)) {
       createSession();
     } else {
-      delete this;
+      deleteLater();
     }
   }
 
