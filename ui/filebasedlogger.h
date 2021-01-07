dbeea27484deac3dd3ebdf56886a3fddc03c6949
Daniel Kamil Kozar <dkk089@gmail.com>
2021-01-07 20:39:23 +0100

    Fix build with Qt 5.15 (#99)

--- ui/filebasedlogger.h
+++ ui/filebasedlogger.h
@@ -11,11 +11,15 @@
 
 // Qt containers do not accept move-only objects by design (QTBUG-59188), which
 // is why we need to use std containers here.
+// however support for std::hash for QStrings was added in 5.15 (QTBUG-33428),
+// so a version check is needed to avoid a duplicate definition.
+#if QT_VERSION < QT_VERSION_CHECK(5, 15, 0)
 namespace std {
 template <> struct hash<QString> {
   std::size_t operator()(const QString &s) const noexcept { return qHash(s); }
 };
 } // namespace std
+#endif
 
 struct AppSettings;
 class QFileInfo;
