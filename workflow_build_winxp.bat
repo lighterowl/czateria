6e2a4ab2e6b80bbbad1de748d0df2188d8c03eea
Daniel Kamil Kozar <dkk089@gmail.com>
2022-01-16 18:40:29 +0100

    fixes for winxp

--- workflow_build_winxp.bat
+++ workflow_build_winxp.bat
@@ -10,5 +10,14 @@ set LIB=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Lib;%LIB%
 
 mkdir build
 cd build
-"C:\Qt\5.6.3-Static-XP\bin\qmake.exe" ..
-nmake release
+rem unfortunately the cmake module files shipped with qt 5.6.3 don't include
+rem external dependencies, or I don't know how to enable them. this is the only
+rem reason why UI_EXTRA_QT_LIBS exist : to pass missing library dependencies to
+rem the executable's target_link_libraries. back when qmake was used, this was
+rem hidden inside .prl files for Qt5Xyz, so everything worked out of the box.
+cmake -DCMAKE_PREFIX_PATH=C:\Qt\5.6.3-Static-XP\lib\cmake^
+  -GNinja -DCMAKE_BUILD_TYPE=Release -DTARGET_WINDOWS_XP=TRUE^
+  -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded^
+  -DUI_EXTRA_QT_LIBS=C:\Qt\5.6.3-Static-XP\lib\libeay32.lib;C:\Qt\5.6.3-Static-XP\lib\qtpcre.lib;C:\Qt\5.6.3-Static-XP\lib\ssleay32.lib;ws2_32;crypt32;dnsapi;iphlpapi^
+  ..
+ninja
