66cfbe87973f05f8b5daab9d86aba714c92ba94c
Daniel Kamil Kozar <dkk089@gmail.com>
2022-01-16 19:19:14 +0100

    xyz

--- .github/workflow_build_windows.bat
+++ .github/workflow_build_windows.bat
@@ -1,11 +1,8 @@
-powershell -Command "Invoke-WebRequest http://download.qt.io/official_releases/jom/jom.zip -O jom.zip"
-7z x jom.zip
-
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 for %%g in (Debug,Release) DO (
   mkdir build%%g
   cd build%%g
   cmake -DCMAKE_BUILD_TYPE=%%g -G "NMake Makefiles" ..
-  ..\jom.exe all
+  nmake
   cd ..
 )
