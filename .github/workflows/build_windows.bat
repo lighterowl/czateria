e6114276f96e1534a427eb0fd01142e415e5840f
Daniel Kamil Kozar <dkk089@gmail.com>
2022-04-05 21:29:17 +0200

    so i guess that vcvars64 was there for a reason after all

--- .github/workflows/build_windows.bat
+++ .github/workflows/build_windows.bat
@@ -1,4 +1,7 @@
-call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
+rem compiling as 32-bit has problems with this version of Qt and msvc post 2017u6 (QTBUG-67259)
+rem so we compile as 64-bit seeing how the xp build is 32 by definition
+
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 mkdir build
 cd build
 powershell -Command "Invoke-WebRequest http://download.qt.io/official_releases/jom/jom.zip -O jom.zip"
